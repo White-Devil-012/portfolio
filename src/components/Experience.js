22f144e4405a0fe1a1e089b1f02f77f1554cfa69
Piyush Kumar <ayushpiyush419@gmail.com>
2024-07-18 20:52:19 +0530

    Added Microsoft experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,33 +8,33 @@ import { motion, useAnimation, useInView } from 'framer-motion';
 import { useEffect, useRef } from "react";
 
 const leftVariants = {
-    hidden: { x: "-100px", opacity: 0 },
-    visible: { x: 0, opacity: 1 }
-    
+  hidden: { x: "-100px", opacity: 0 },
+  visible: { x: 0, opacity: 1 }
+
 }
 const rightVariants = {
-    hidden: { x: "100px", opacity: 0 },
-    visible: { x: 0, opacity: 1 }
-    
+  hidden: { x: "100px", opacity: 0 },
+  visible: { x: 0, opacity: 1 }
+
 }
 
 export const Experience = () => {
 
-    const aRef = useRef(null);
-    const isInView = useInView(aRef, {once: true});
-    const leftControl = useAnimation();
-    const rightControl = useAnimation();
-    useEffect(() => {
-      if(isInView) {
-        leftControl.start("visible");
-        rightControl.start("visible");
-      }
-    }, [isInView])
-    
+  const aRef = useRef(null);
+  const isInView = useInView(aRef, { once: true });
+  const leftControl = useAnimation();
+  const rightControl = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      leftControl.start("visible");
+      rightControl.start("visible");
+    }
+  }, [isInView])
+
   return (
     <section className="experience" id="experience">
       <div className="container">
-        <h2><img className = "naruto-title" src={expTitle}></img></h2>
+        <h2><img className="naruto-title" src={expTitle}></img></h2>
         <div ref={aRef} className="experience-items">
           <motion.div
             className="experience-item"
@@ -43,29 +43,36 @@ export const Experience = () => {
             animate={leftControl}
             transition={{ duration: 0.5 }}
           >
-                <img src={microsoftLogo} alt="Microsoft" className="company-logo" />
-                <div className="experience-details">
-                    <h3>Microsoft</h3>
-                    <p>Upcoming SWE Intern</p>
-                    { /* <p>Date Range 1</p>
-                    <p>Description of responsibilities and achievements.</p> */}
-                </div>
+            <img src={microsoftLogo} alt="Microsoft" className="company-logo" />
+            <div className="experience-details">
+              <h3>Microsoft</h3>
+              <p>Software Engineer Intern</p>
+              <p>May 2024 - July 2024</p>
+              <ul>
+                <li>Developed DynaAssist, a browser extension integrated with Dynamics 365, to enhance user experience by automating
+                  repetitive tasks.</li>
+                <li>Utilized the OpenAI API with GPT-4 to enable users to perform actions through simple text prompts.</li>
+                <li>Designed features to allow ad-hoc instructions and perform memory leak testing.</li>
+              </ul>
+            </div>
           </motion.div>
           <motion.div
             className="experience-item"
-            variants={rightVariants}
+            variants={leftVariants}
             initial="hidden"
-            animate={rightControl}
+            animate={leftControl}
             transition={{ duration: 0.5 }}
           >
-                <img src={tecnoesisLogo} alt="Tecnoesis" className="company-logo" />
-                <div className="experience-details">
-                    <h3>Tecnoesis</h3>
-                    <p>Front-end Developer</p>
-                    <p>October 2022 - Present</p>
-                    <p>Collaborated with a team of web developers to contribute to the design and implementation of the Tecnoesis
-website.</p>
-                </div>
+            <img src={tecnoesisLogo} alt="Tecnoesis" className="company-logo" />
+            <div className="experience-details">
+              <h3>Tecnoesis</h3>
+              <p>Front-end Developer</p>
+              <p>October 2022</p>
+              <ul>
+              <li>Took an important role in front-end development tasks.</li>
+              <li>Ensured a visually appealing and user-friendly interface for the fest’s online presence.</li>
+              </ul>
+            </div>
           </motion.div>
         </div>
       </div>
